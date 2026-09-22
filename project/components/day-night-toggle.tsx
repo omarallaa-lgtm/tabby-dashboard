87d0c5035b40258e3e97333eea13a7264efd3e74
omarallaa-lgtm <omar.allaa@tabby.ai>
2026-09-22 21:12:27 +0300

    Update cloud images and comments in day-night toggle

--- project/components/day-night-toggle.tsx
+++ project/components/day-night-toggle.tsx
@@ -55,18 +55,18 @@ export function DayNightToggle({ isDarkMode, onToggle }: DayNightToggleProps) {
           </svg>
         </div>
 
-        {/* Moving Cloud Track using cloud.png */}
+        {/* Moving Cloud Infinite Track */}
         <div className="clouds">
           <div className="cloud-track">
-            <img src="/cloud.png" alt="" aria-hidden="true" draggable={false} />
-            <img src="/cloud.png" alt="" aria-hidden="true" draggable={false} />
-            <img src="/cloud.png" alt="" aria-hidden="true" draggable={false} />
-            <img src="/cloud.png" alt="" aria-hidden="true" draggable={false} />
+            <img src="/cloud2.png" alt="" aria-hidden="true" draggable={false} />
+            <img src="/cloud2.png" alt="" aria-hidden="true" draggable={false} />
+            <img src="/cloud2.png" alt="" aria-hidden="true" draggable={false} />
+            <img src="/cloud2.png" alt="" aria-hidden="true" draggable={false} />
           </div>
         </div>
       </div>
 
-      {/* Sliding Knob */}
+      {/* Knob */}
       <div className="knob"></div>
     </button>
   );
